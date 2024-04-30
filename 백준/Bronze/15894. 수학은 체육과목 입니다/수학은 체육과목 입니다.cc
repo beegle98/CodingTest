adeedf8becbd48f360c188b943c418effc831a1b
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-04-30 23:14:41 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+  
+using namespace std;
+#define FASTIO cin.tie(0);cout.tie(0);ios::sync_with_stdio(false);
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+
+
+
+int main(){
+    FASTIO
+ 
+    ll n; cin >> n;
+    cout << 4*n;
+    return 0;
+}
