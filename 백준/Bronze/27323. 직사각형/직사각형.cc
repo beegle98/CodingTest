03176cc766481eb926d50d65886b580db37302a8
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-05-05 21:53:00 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+  
+using namespace std;
+#define FASTIO cin.tie(0);ios::sync_with_stdio(false);
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+
+int main(){
+    FASTIO
+ 
+    int a,b; cin >> a >> b;
+    cout << a*b;
+    
+    return 0;
+}
