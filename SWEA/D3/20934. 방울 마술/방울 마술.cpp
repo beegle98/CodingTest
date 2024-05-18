1ac5ab2c98791601a9453e289285c7e6c8e9bde5
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-05-18 22:43:50 +0900

    [D3] Title: 방울 마술, Time: 8 ms, Memory: 13,376 KB -BaekjoonHub

--- SWEA/D3/20934. 방울 마술/방울 마술.cpp
+++ SWEA/D3/20934. 방울 마술/방울 마술.cpp
@@ -1,12 +1,7 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 #define FASTIO cin.tie(0);ios::sync_with_stdio(false);
-typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-typedef pair<ll,ll> pll;
 
 int main(){
     FASTIO
