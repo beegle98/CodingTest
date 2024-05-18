4f0ebd26e2087c5812b8e72f2a0891a896ce1f3c
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-05-18 19:57:04 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO cin.tie(0);ios::sync_with_stdio(false);
+
+int main(){
+    FASTIO
+    int n; cin >> n;
+    if(n%2==1) cout << "SK";
+    else cout << "CY";
+    return 0;
+}
+
