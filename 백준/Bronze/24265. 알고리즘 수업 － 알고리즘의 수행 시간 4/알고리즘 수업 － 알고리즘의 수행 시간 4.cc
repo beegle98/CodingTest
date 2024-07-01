2220c45a53b10d6d32c81259f979c2cb0d69e1d0
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-07-01 18:48:33 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO  cin.tie(0);ios::sync_with_stdio(false);
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+
+int main()
+{
+    ll n; cin >> n;
+    cout << n*(n-1)/2 << "\n2";
+    return 0;
+}
