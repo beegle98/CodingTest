54f8cfb82a4e882d5d214cd22e948099968cda4b
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-07-10 00:01:39 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 92 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO  cin.tie(0);ios::sync_with_stdio(false);
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+queue<int> q;
+vi s[100001];
+int cnt=1, ans[100001];
+
+void bfs(int v){
+    if(ans[v]) return; 
+    ans[v]=cnt++;
+    for(int i=0;i<s[v].size();i++){
+        q.push(s[v][i]);
+    }
+}
+
+int main()
+{
+    FASTIO
+    int n,m,r; cin >> n >> m >> r;
+    for(int i=0;i<m;i++){
+        int x,y; cin >> x >> y;
+        s[x].push_back(y);
+        s[y].push_back(x);
+    }
+    for(int i=1;i<=n;i++) sort(s[i].begin(),s[i].end(),greater<int>());
+    q.push(r);
+    while(!q.empty()){
+        int tmp = q.front();
+        q.pop();
+        bfs(tmp);
+    }
+    for(int i=1;i<=n;i++){
+        cout << ans[i] << '\n';
+    }
+    return 0;
+}
