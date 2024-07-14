74c8d4e9436d6158c2c0e0ed62c976a477f18c93
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-07-14 22:03:08 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define FASTIO  cin.tie(0);ios::sync_with_stdio(false);
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+
+int main()
+{
+    FASTIO
+    cout << "    8888888888  888    88888\n";
+    cout << "   88     88   88 88   88  88\n";
+    cout << "    8888  88  88   88  88888\n";
+    cout << "       88 88 888888888 88   88\n";
+    cout << "88888888  88 88     88 88    888888\n\n";
+    cout << "88  88  88   888    88888    888888\n";
+    cout << "88  88  88  88 88   88  88  88\n";
+    cout << "88 8888 88 88   88  88888    8888\n";
+    cout << " 888  888 888888888 88  88      88\n";
+    cout << "  88  88  88     88 88   88888888\n";
+    return 0;
+
+}
