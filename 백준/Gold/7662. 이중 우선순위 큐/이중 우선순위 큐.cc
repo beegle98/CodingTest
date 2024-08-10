c04bc1a7fe0137c378ad50f4a66ff003925a90d0
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-10 21:33:10 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1408 ms, Memory: 65292 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,49 +1,74 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
 
 using namespace std;
-#define FASTIO  cin.tie(0);ios::sync_with_stdio(false);
-typedef long long ll;
-typedef vector<ll> vll;
-typedef vector<int> vi;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pll;
-#define all(v) v.begin(), v.end()
 
-multiset<int> dual_pq;
+int k;
+vector<pair<char, int>> command;
+map<int, int> cnt;
 
-int main()
-{
-    FASTIO
-    int tc;
-    cin >> tc;
-    for (int t = 1; t <= tc;t++){
-        int n;
-        cin >> n;
-        for (int i = 0; i < n;i++){
-            char c;
-            cin >> c;
-            int x;
-            cin >> x;
-            if(c=='I'){
-                dual_pq.insert(x);
-            }
-            else if(c=='D'){
-                if(dual_pq.empty()) continue;
-                if(x==1){ // 최댓값 제거
-                    dual_pq.erase(--dual_pq.end());
-                }else if(x==-1){// 최솟값 제거
-                    dual_pq.erase(dual_pq.begin());
+void solution() {
+    priority_queue<int, vector<int>, less<int>> up;
+    priority_queue<int, vector<int>, greater<int>> lo;
+
+    for (int i = 0; i < command.size(); i++) {
+        char c = command[i].first;
+        int n = command[i].second;
+
+        if (c == 'I') {
+            up.push(n);
+            lo.push(n);
+            cnt[n]++;
+        }
+        else {
+            if (n == 1) {
+                if (!up.empty()) {
+                    cnt[up.top()]--;
+                    up.pop();
                 }
-            }else {
-                continue;
             }
+            else {
+                if (!lo.empty()) {
+                    cnt[lo.top()]--;
+                    lo.pop();
+                }
+            }
+            while (!up.empty() && cnt[up.top()] == 0) up.pop();
+            while (!lo.empty() && cnt[lo.top()] == 0) lo.pop();
         }
-        if(dual_pq.empty()){
-            cout << "EMPTY\n";
-        }else{
-            cout << *(--dual_pq.end()) << ' ' << *dual_pq.begin() << '\n';
-        }
-        dual_pq.clear();
     }
+
+    while (!up.empty() && cnt[up.top()] == 0) up.pop();
+    while (!lo.empty() && cnt[lo.top()] == 0) lo.pop();
+
+    if (up.empty() || lo.empty()) cout << "EMPTY" << "\n";
+    else cout << up.top() << " " << lo.top() << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        command.clear();
+        cnt.clear();
+
+        cin >> k;
+
+        char c;
+        int n;
+        for (int i = 0; i < k; i++) {
+            cin >> c >> n;
+
+            command.push_back({ c,n });
+        }
+
+        solution();
+    }
+
     return 0;
 }
