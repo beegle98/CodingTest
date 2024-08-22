1dd030e3b4f866df8d8028ac2d1b6a64003dbb16
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-23 00:17:39 +0900

    [D4] Title: 정사각형 방, Time: 159 ms, Memory: 31,168 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import javax.management.loading.PrivateClassLoader;
+
+public class Solution {
+	static int N;
+	static int[][] MAP;
+	static boolean[][] visited;
+	static int Min, Max;
+	static int[] dx= {0,0,1,-1};
+	static int[] dy= {1,-1,0,0};
+	
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			MAP = new int[N][N];
+			visited = new boolean[N][N];
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					MAP[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			Max = 0;
+			Min = Integer.MAX_VALUE;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (visited[i][j]) continue;
+					else {
+						solve(i,j);
+					}
+				}
+			}
+			System.out.println("#" + tc + " " + Min + " " + Max);
+		}
+	}
+	private static void solve(int x,int y) {
+		int cnt = dfsUp(x,y,1);
+		dfsDown(x,y,cnt);
+	}
+	
+	private static int dfsUp(int x,int y, int cnt) {
+		visited[x][y]=true;
+		for(int i=0;i<4;i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if(nx >=0 && nx < N && ny >=0 && ny < N
+					&& !visited[nx][ny] && MAP[x][y] + 1 == MAP[nx][ny]) {
+				return dfsUp(nx,ny,cnt+1);
+			}
+		}
+		return cnt;
+	}
+	private static void dfsDown(int x,int y,int cnt) {
+		visited[x][y]=true;
+		for(int i=0;i<4;i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if(nx >=0 && nx < N && ny >=0 && ny < N
+					&& !visited[nx][ny] && MAP[x][y] - 1 == MAP[nx][ny]) {
+				dfsDown(nx,ny,cnt+1);
+			}
+		}
+		if(cnt>Max) {
+			Max = cnt;
+			Min = MAP[x][y];
+		}else if(cnt==Max) {
+			Min = Math.min(Min, MAP[x][y]);
+		}
+	}
+}
