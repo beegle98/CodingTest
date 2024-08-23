a3794811eb612a61086c2fa0fe876584c2332df1
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-23 15:15:44 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 456 ms, Memory: 61000 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,73 +1,83 @@
-import java.io.*;
-import java.util.*;
- 
-public class Main {
-    static int N, M, K;
-    static int[][] MAP;
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {-1,1,0,0};
-    static int[] hx = {-2,-2,-1,-1,1,1,2,2};
-    static int[] hy = {-1,1,-2,2,-2,2,-1,1};
-    static int ans;
-    static boolean[][][] visited;
-    public static void main(String[] args) throws IOException {
-         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		N = Integer.parseInt(st.nextToken());
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main{//_1600_G3_말이_되고픈_원숭이_이현지 {
+	static int K, W, H, move, k, ans;
+	static int[][] map;
+	static boolean[][][] visited;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+    static int[] hdr = {-2, -2, -1, -1, 1, 1, 2, 2};
+    static int[] hdc = {-1, 1, -2, 2, -2, 2, -1, 1};
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		MAP = new int[N][M];
-		visited = new boolean[N][M][K+1];
-		for (int i = 0; i < N; i++) {
+		K = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		map = new int[H][W];
+		visited = new boolean[K+1][H][W];
+		
+		for(int i=0; i<H; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++)
-				MAP[i][j] = Integer.parseInt(st.nextToken());
+			for(int j=0; j<W; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
 		}
-		ans=-1;
-		bfs();
-		System.out.println(ans);
-    }
-    private static class Pos{
-    	int x;
-    	int y;
-    	int cnt;
-    	int horse;
-		public Pos(int x, int y, int cnt, int horse) {
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-			this.horse = horse;
+		
+		
+		move = 0; k=0; ans = Integer.MAX_VALUE;
+		
+		System.out.println(bfs(0,0,0,0));
+	}
+
+	private static int bfs(int h, int w, int k, int move) {
+		Deque<int[]> q = new ArrayDeque<>();
+		q.add(new int[] {h, w, k, move});  
+		visited[k][h][w] = true;
+		
+		while(!q.isEmpty()) {
+			int[] tmp = q.poll();
+			h=tmp[0]; w = tmp[1]; k = tmp[2]; move = tmp[3];
+			
+			if(h==H-1 && w==W-1) { // 도착
+				ans = Math.min(ans, move);
+				return ans;
+			}
+			
+			int nh, nw;
+			for(int i=0; i<4; i++) {
+				nh = h + dr[i];
+				nw = w + dc[i];
+				if( nh<0 || H-1<nh || nw<0 || W-1<nw || map[nh][nw] == 1 || visited[k][nh][nw] == true) {
+					continue;
+				}
+				visited[k][nh][nw] = true;
+				q.add(new int[] {nh, nw, k, move+1});
+
+			}
+			if(k < K) {
+				for(int i=0; i<8; i++) {
+					nh = h + hdr[i];
+					nw = w + hdc[i];
+					if( nh<0 || H-1<nh || nw<0 || W-1<nw || map[nh][nw] == 1 || visited[k+1][nh][nw] == true) {
+						continue;
+					}
+					visited[k+1][nh][nw] = true;
+					q.add(new int[] {nh, nw, k+1, move+1});
+				}
+			}
 		}
-    }
-    private static void bfs() {
-    	Queue<Pos> q = new LinkedList<>();
-    	q.add(new Pos(0,0,0,0));
-    	visited[0][0][0]=true;
-    	while(!q.isEmpty()){
-    		Pos cur = q.poll();
-    		if(cur.x==N-1 && cur.y==M-1) {
-    			ans = cur.cnt;
-    			q.clear();
-    			break;
-    		}
-    		for(int i=0;i<8;i++) {
-    			if(cur.horse==K) break;
-    			int nx = cur.x + hx[i];
-    			int ny = cur.y + hy[i];
-    			if(nx<0||nx>=N||ny<0||ny>=M|| MAP[nx][ny]==1||visited[nx][ny][cur.horse+1]) continue;
-    			visited[nx][ny][cur.horse+1]=true;
-    			q.add(new Pos(nx,ny,cur.cnt+1,cur.horse+1));
-    		}
-    		for(int i=0;i<4;i++) {
-    			int nx = cur.x + dx[i];
-    			int ny = cur.y + dy[i];
-    			if(nx<0||nx>=N||ny<0||ny>=M||MAP[nx][ny]==1||visited[nx][ny][cur.horse]) continue;
-    			visited[nx][ny][cur.horse]=true;
-    			q.add(new Pos(nx,ny,cur.cnt+1,cur.horse));
-    		}
-    	}
-    }
+		
+		return -1;
+	}
+	
 }
