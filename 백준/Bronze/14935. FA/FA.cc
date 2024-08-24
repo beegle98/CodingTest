13259b8584a81dc7a8617b7b3fef1aa4c6b26c29
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-24 22:07:55 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+
+int main() {
+	FASTIO
+	string s; cin >> s;
+	cout << "FA";
+	return 0;
+}
