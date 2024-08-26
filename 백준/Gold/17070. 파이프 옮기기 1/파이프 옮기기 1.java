540f0817b2b66a113a39bac8d42927b5bf2563ec
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-26 14:53:11 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 100 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -3,28 +3,31 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-		static int N,ans;
+		static int N;
+		static long ans;
 		static int[][] MAP;
 		static int[][] dx = {{0,1},{1,1}};
 		static int[][] dy = {{1,1},{0,1}};
 		static int[][] ds = {{0,2},{1,2}};
 		static int[][] cross = {{0,1},{1,0},{1,1}};
 		static boolean[][] visited;
+		static long[][][] dp = new long[40][40][3];//x,y,모양 (가로:0, 세로:1, 대각선:2)
 		
     public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	StringTokenizer st = new StringTokenizer(br.readLine());
     	
     	N = Integer.parseInt(st.nextToken());
-    	MAP = new int[N][N];
-    	visited = new boolean[N][N];
-    	for(int i=0;i<N;i++) {
+    	MAP = new int[N+1][N+1];
+    	visited = new boolean[N+1][N+1];
+    	for(int i=1;i<=N;i++) {
     		st = new StringTokenizer(br.readLine());
-    		for(int j=0;j<N;j++) MAP[i][j]=Integer.parseInt(st.nextToken());
+    		for(int j=1;j<=N;j++) MAP[i][j]=Integer.parseInt(st.nextToken());
     	}
     	ans=0;
-    	visited[1][1]=true;
-    	dfs(0,1,0);
+    	//visited[1][1]=true;
+    	//dfs(0,1,0);
+    	ans = dp();
     	System.out.println(ans);
     }
     // stat : 0=가로, 1=세로, 2=대각선
@@ -40,7 +43,7 @@ public class Main {
         	for(int i=0;i<3;i++) {
         		nx = x + cross[i][0];
         		ny = y + cross[i][1];
-        		if(nx<0 || nx >=N || ny<0 || ny>=N || visited[nx][ny] || MAP[nx][ny]==1) continue;
+        		if(nx<1 || nx >N || ny<1 || ny>N || visited[nx][ny] || MAP[nx][ny]==1) continue;
         		if(i==2 && (MAP[nx-1][ny]==1 || MAP[nx][ny-1]==1)) continue;
         		visited[nx][ny]=true;
         		dfs(nx,ny,i);
@@ -51,12 +54,30 @@ public class Main {
     			nx = x+dx[stat][i];
     			ny = y+dy[stat][i];
     			ns = ds[stat][i];
-    			if(nx<0 || nx >=N || ny<0 || ny>=N || visited[nx][ny] || MAP[nx][ny]==1) continue;
+    			if(nx<1 || nx >N || ny<1 || ny>N || visited[nx][ny] || MAP[nx][ny]==1) continue;
     			if(i==1 && (MAP[nx-1][ny]==1 || MAP[nx][ny-1]==1)) continue;
     			visited[nx][ny]=true;
     			dfs(nx,ny,ns);
     			visited[nx][ny]=false;
     		}
     	}
+    }
+    private static long dp() {
+    	dp[1][2][0] = 1;
+    	for (int i = 1; i <= N; i++) {
+    		for (int j = 1; j <= N; j++) {
+    			if (MAP[i][j] == 1) continue;
+    			dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
+    			dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
+    			if (MAP[i - 1][j] == 0 && MAP[i][j - 1] == 0) {
+    				for (int k = 0; k < 3; k++) dp[i][j][2] += dp[i - 1][j - 1][k];
+    			}
+    		}
+    	}
+    	long res = 0;
+    	for (int i = 0; i < 3; i++) {
+    		res += dp[N][N][i];
+    	}
+    	return res;
     }
 }
