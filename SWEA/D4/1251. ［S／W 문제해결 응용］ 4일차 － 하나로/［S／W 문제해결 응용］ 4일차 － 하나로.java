b2ff21764845cf8286761ba7bbf0b2c2907195f3
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-30 11:34:53 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 208 ms, Memory: 30,784 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,26 +1,32 @@
 import java.io.*;
 import java.util.*;
+/*
+크루스칼
+91,496 kb
+메모리
+561 ms
+실행시간
 
+프림
+
+ */
 public class Solution {
-
-	private static class Edge implements Comparable<Edge>{
-		int start, end;
-		long weight;
-
-		public Edge(int start, int end, long weight) {
+	static class Node implements Comparable<Node>{
+		int idx;
+		long cost;
+		public Node(int idx, long cost) {
 			super();
-			this.start = start;
-			this.end = end;
-			this.weight = weight;
+			this.idx = idx;
+			this.cost = cost;
 		}
-
 		@Override
-		public int compareTo(Edge o) {
-			return Long.compare(this.weight, o.weight);
+		public int compareTo(Node o) {
+			return Long.compare(this.cost, o.cost);
 		}
+		
 	}
-	static int V;
-	static int[] parents;
+	static final long INF = Long.MAX_VALUE;
+	static int N;
 	static long[][] list;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -29,34 +35,19 @@ public class Solution {
 		for(int tc=1;tc<=T;tc++) {
 			sb.append("#" + tc + " ");
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			parents = new int[V];
-			ArrayList<Edge> edges = new ArrayList<>();
-			list = new long[V][2];
+			N = Integer.parseInt(st.nextToken());
+			
+			list = new long[N][2];
 			for(int i=0;i<2;i++) {
 				st = new StringTokenizer(br.readLine());
-				for(int j=0;j<V;j++) {
+				for(int j=0;j<N;j++) {
 					list[j][i] = Integer.parseInt(st.nextToken());
 				}
 			}
 
 			double E = Double.parseDouble(br.readLine());
-			for(int i=0;i<V;i++) {
-				for(int j=i+1;j<V;j++) {
-					long weight = len(i,j);
-					edges.add(new Edge(i, j, weight));
-				}
-			}
-			Collections.sort(edges); //간선의 가중치 기준 오름차순 정렬
-			make(); // 모든 정점을 분리집합으로 만듬 (단위 서로소 집합<트리> 생성)
-			int cnt=0;
-			long cost=0;
-			for(Edge edge : edges) {
-				if(union(edge.start, edge.end)) {
-					cost += edge.weight;
-					if(++cnt == V-1) break; // MST 완성!!
-				}
-			}
+			
+			long cost = prim();
 			double ans;
 			ans = E * cost;
 			sb.append(String.format("%.0f",ans) + "\n");
@@ -66,21 +57,31 @@ public class Solution {
 	private static long len(int i, int j) {
 		return (list[i][0]-list[j][0])*(list[i][0]-list[j][0]) + (list[i][1]-list[j][1])*(list[i][1]-list[j][1]);
 	}
-	private static void make() {
-		for(int i=0;i<V;i++) {
-			parents[i]=i;
+	private static long prim() {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		boolean[] visited = new boolean[N];
+		long[] Dist = new long[N];
+		Arrays.fill(Dist, INF);
+		Dist[0]=0;
+		pq.add(new Node(0,0));
+		long sum = 0;
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+			if(visited[cur.idx]) continue;
+			
+			sum += cur.cost;
+			visited[cur.idx]=true;
+			
+			for(int i=0;i<N;i++) {
+				if(visited[i]) continue;
+				long dist = len(cur.idx,i);
+				if(dist < Dist[i]) {
+					Dist[i] = dist;
+					pq.add(new Node(i,dist));
+				}
+			}
 		}
-	}
-	private static int findSet(int a) {
-		if(parents[a]==a) return a;
-		return parents[a] = findSet(parents[a]); // 경로 압축!!
-	}
-	static boolean union(int a,int b) {
-		a = findSet(a);
-		b = findSet(b);
-		if(a==b) return false;
-		if(a>b) parents[a] = b;
-		else parents[b] = a;
-		return true;
+		return sum;
 	}
 }
