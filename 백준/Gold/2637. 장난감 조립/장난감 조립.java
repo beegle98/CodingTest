56f3ca411b57271a32eea02d966850538be4b6b7
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-08-30 16:16:50 +0900

    [Gold II] Title: 장난감 조립, Time: 120 ms, Memory: 16100 KB -BaekjoonHub

--- 백준/Gold/2637. 장난감 조립/장난감 조립.java
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.java
@@ -1,80 +1,53 @@
-import java.io.*;
-import java.lang.*;
 import java.util.*;
+import java.io.*;
 
-/*
- * 306012kb	1220ms
- */
-
-public class Main{//_2637_G2_장난감_조립_이현지 {
-    static int n, m, x, y, k;
-    static int[] arr;
-    static boolean[] mid;
-    static class Edge{
-    	int from, to, weight;
-
-		public Edge(int from, int to, int weight) {
-			super();
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
+public class Main {
+	
+	static int N,M;
+	static long[][] dp;
+	static int[][] map;
+	static int[] outDegree;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		dp = new long[N+1][N+1];
+		map = new int[N+1][N+1];
+		outDegree = new int[N+1];
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a,b,c;
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			outDegree[a]++;
+			map[a][b] = c;
 		}
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        
-        // count 계산해서 저장
-        arr = new int[n+1];
-        arr[n] = 1;
-        mid = new boolean[n+1];
-        
-        // 진출 배열
-        List<Edge>[] list = new ArrayList[n+1];
-        for(int i=0; i<n+1; i++) {
-        	list[i] = new ArrayList<>();
-        }
-        
-        // 진입 차수 배열
-        int[] inDegree = new int[n+1];
-        
-        // 진입차수 0 노드 큐
-        LinkedList<Integer> q = new LinkedList<>();
-        
-        StringTokenizer st;
-        for(int i=0; i<m; i++) {
-        
-        	st = new StringTokenizer(br.readLine());
-        	x = Integer.parseInt(st.nextToken());
-        	y = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-            
-            mid[x] = true;
-            list[x].add(new Edge(x, y, k));
-            inDegree[y]++;
-        }
-        // 다른 노드가 필요로 하지 않는 거 먼저!
-        for(int i=1; i<n+1; i++) {
-        	if(inDegree[i] == 0) q.offer(i);
-        }
-        
-        // 큐에서 뺴면서 계산
-        while(!q.isEmpty()) {
-        	int cur = q.poll();
-        	for(Edge edge : list[cur]) {
-        		arr[edge.to] += arr[cur] * edge.weight;
-//        		System.out.println(Arrays.toString(arr));
-        		if(--inDegree[edge.to] == 0) {
-        			q.offer(edge.to);
-        		}
-        	}
-        }
-        for(int i=1; i<n+1; i++) {
-        	if(!mid[i]) {
-        		System.out.println(i+" "+arr[i]);
-        	}
-        }
-    }
+		for(int i=1;i<N;i++) {
+			if(outDegree[i]==0) {
+				dp[i][0]=1;
+				dp[i][i]=1;
+			}
+		}
+		recur(N);
+		for(int i=1;i<N;i++) {
+			if(outDegree[i]==0) {
+				System.out.println(i + " " + dp[N][i]);
+			}
+		}
+	}
+	private static long[] recur(int idx) {
+		if(dp[idx][0]>0) return dp[idx];
+		for(int i=1;i<N;i++) {
+			int cnt = map[idx][i];
+			if(cnt>0) {
+				long[] tmp = recur(i);
+				for(int j=0;j<N;j++) {
+					dp[idx][j] += cnt * tmp[j];
+				}
+			}
+		}
+		return dp[idx];
+	}
 }
-
