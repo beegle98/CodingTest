58cabe20b344a57b2212cdd4484fde215d91150b
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-09-02 11:42:18 +0900

    [Silver II] Title: 외판원 순회 2, Time: 112 ms, Memory: 19304 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static final int MAX = 1 << 16;
+    private static final int INF = 987654321;
+
+    private static int[][] w = new int[20][20];
+    private static int n, fullBit;
+    private static int[][] dp = new int[20][MAX];
+
+    private static int dfs(int cur, int visited) {
+        if (visited == fullBit) {
+            if (w[cur][0] != 0) {
+                return w[cur][0];
+            } else {
+                return INF;
+            }
+        }
+
+        if (dp[cur][visited] != -1) return dp[cur][visited];
+
+        dp[cur][visited] = INF;
+        for (int i = 0; i < n; i++) {
+            if (w[cur][i] == 0 || (visited & (1 << i)) != 0) continue;
+            dp[cur][visited] = Math.min(dp[cur][visited], w[cur][i] + dfs(i, visited | (1 << i)));
+        }
+
+        return dp[cur][visited];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        fullBit = (1 << n) - 1;
+
+        for (int[] i : dp) {
+            java.util.Arrays.fill(i, -1);
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                w[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        System.out.println(dfs(0, 1));
+    }
+}
