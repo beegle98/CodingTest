de20e5ecac80861b831b2255c6507f014c02e4c8
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-09-02 16:12:18 +0900

    [Silver II] Title: 외판원 순회 2, Time: 456 ms, Memory: 19268 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.java
@@ -1,55 +1,89 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
+//
 
-    private static final int MAX = 1 << 16;
-    private static final int INF = 987654321;
-
-    private static int[][] w = new int[20][20];
-    private static int n, fullBit;
-    private static int[][] dp = new int[20][MAX];
-
-    private static int dfs(int cur, int visited) {
-        if (visited == fullBit) {
-            if (w[cur][0] != 0) {
-                return w[cur][0];
-            } else {
-                return INF;
-            }
-        }
-
-        if (dp[cur][visited] != -1) return dp[cur][visited];
-
-        dp[cur][visited] = INF;
-        for (int i = 0; i < n; i++) {
-            if (w[cur][i] == 0 || (visited & (1 << i)) != 0) continue;
-            dp[cur][visited] = Math.min(dp[cur][visited], w[cur][i] + dfs(i, visited | (1 << i)));
-        }
-
-        return dp[cur][visited];
+public class Main{//_10971_S2_외판원_순회_2_이현지 {
+    static int N, ans, cnt, min;
+    static boolean[] visited;
+    static ArrayList<Edge> edges;
+    static Edge cur;
+    
+    static public class Edge implements Comparable<Edge>{
+    	int from, to, weight;
+		public Edge(int from, int to, int weight) {
+			super();
+			this.from = from;
+			this.to = to;
+			this.weight = weight;
+		}
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight-o.weight;
+		}
+		@Override
+		public String toString() {
+			return "Edge [from=" + from + ", to=" + to + ", weight=" + weight + "]";
+		}
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        fullBit = (1 << n) - 1;
-
-        for (int[] i : dp) {
-            java.util.Arrays.fill(i, -1);
+        N = Integer.parseInt(br.readLine());
+        
+        edges = new ArrayList<Edge>();
+        StringTokenizer st;
+        for(int i=0; i<N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int j=0; j<N; j++) {
+        		int x = Integer.parseInt(st.nextToken());
+        		if(x!=0)
+        			edges.add(new Edge(i,j,x));
+        	}
         }
+        //Collections.sort(edges);
+        min = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                w[i][j] = Integer.parseInt(st.nextToken());
+        for(Edge e : edges) {
+            if(e.from == 0) {
+                cnt = 1;
+                visited = new boolean[N];
+                ans = e.weight;
+                cur = e;
+                //        			System.out.println(cur.toString());
+                visited[cur.to]= true;
+                search(cur,cnt,visited,ans);
             }
         }
-
-        System.out.println(dfs(0, 1));
+        
+        System.out.println(min);
+    }
+    public static void search(Edge edge, int cnt, boolean[] visited, int ans) {
+//    	for(int i=0; i<N; i++) {
+//    		System.out.print(visited[i]+" ");
+//    	}
+//    	System.out.println(ans);
+    	
+    	if(cnt == N-1) {
+    		for(Edge e : edges) {
+        		if(edge.to == e.from && e.to == cur.from) {
+//        			System.out.println(e.toString());
+        			min = Math.min(min, ans+e.weight);
+//        			System.out.println("min: "+min);
+//                    return;
+        		}
+    		}
+    		return;
+    	}
+    	for(Edge e : edges) {
+    		if(e.from == edge.to && e.to!=edge.from && e.to!=cur.from && !visited[e.to]) {
+//    			System.out.println(e.toString());
+    			visited[e.to] = true;
+    			search(e, cnt+1, visited, ans+e.weight);
+    			visited[e.to] = false;
+    		}
+    	}
+        return;
     }
 }
+
