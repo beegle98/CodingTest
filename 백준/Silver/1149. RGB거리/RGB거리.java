9030bd9b579014bcc821b3cf78d9511d46c71afd
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-09-04 16:33:18 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14572 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,27 +1,38 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int[] cur = new int[3];
-        int[] next = new int[3];
-        
-    	for (int i = 0; i < N; i++){
-    		st = new StringTokenizer(br.readLine());
-    		for (int j=0;j<3;j++) {
-    			next[j] = Integer.parseInt(st.nextToken());
-    			next[j] += Math.min(cur[(j+1)%3], cur[(j+2)%3]);
-    		}
-    		for (int j = 0; j < 3; j++) cur[j] = next[j];
-    	}
-    	int min=Integer.MAX_VALUE;
-    	for(int i=0;i<3;i++) min = Math.min(min,cur[i]);
-    	System.out.println(min);
-    }
-    
+
+public class Main{//_1149_S1_RGB거리_이현지 {
+	static int N;
+	static int[][] table, memo;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		memo = new int[N][3];
+		table = new int[N][3];
+		StringTokenizer st;
+		for(int i=0; i<N; i++) {
+			st= new StringTokenizer(br.readLine());
+			for(int j=0; j<3; j++) {
+				table[i][j] = Integer.parseInt(st.nextToken());
+			}
+			
+		}
+		for(int i=0; i<N; i++) {
+			if(i != 0) {
+				for(int j=0; j<3; j++) {
+					memo[i][j] = Math.min(memo[i-1][(j+1)%3], memo[i-1][(j+2)%3])+table[i][j];
+				}
+			}else {
+				for(int j=0; j<3; j++) {
+					memo[0][j] = table[0][j];
+				}
+			}
+		}
+//		for(int i=0; i<N; i++) {
+//			System.out.println(Arrays.toString(memo[i]));
+//		}
+		System.out.println(Math.min(memo[N-1][0], Math.min(memo[N-1][1], memo[N-1][2])));
+	}
+
 }
