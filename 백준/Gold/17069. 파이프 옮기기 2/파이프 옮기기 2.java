5411038d0374bc5e9441c27fa182416e28c70381
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-09-06 14:42:05 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 112 ms, Memory: 14536 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,40 +1,40 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-    static long[][] s;
-    static long[][][] dp; // (가로: 0, 세로: 1, 대각선: 2)
-    
+public class Main{//_17069_G4_파이프_옮기기_2_이현지 {
+    static int n;
+    static int[][] map;
+    static long[][][] dp;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        s = new long[N+1][N+1];
-        dp = new long[N+1][N+1][3];
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= N; j++) {
-                s[i][j] = Integer.parseInt(st.nextToken());
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        for(int i = 0; i < n; i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dp[1][2][0] = 1;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 2; j <= N; j++) {
-                if (s[i][j] == 1) continue;
-                dp[i][j][0] += dp[i][j - 1][0] + dp[i][j - 1][2];
-                dp[i][j][1] += dp[i - 1][j][1] + dp[i - 1][j][2];
-                if (s[i - 1][j] == 0 && s[i][j - 1] == 0) {
-                    dp[i][j][2] += dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
-                }
-            }
-        }
-        
-        long ans = 0;
-        for (int i = 0; i < 3; i++) {
-            ans += dp[N][N][i];
-        }
-        
-        System.out.println(ans);
+        dp = new long[n][n][3]; // 가로, 대각선, 세로
+        dp[0][1][0]=1;
+        dp();
+        System.out.println(dp[n-1][n-1][0]+dp[n-1][n-1][1]+dp[n-1][n-1][2]);
     }
+
+	private static void dp() {
+		for(int i=0; i<n; i++) {
+			for(int j=1; j<n; j++) {
+				if(i==0 && j==1) continue;
+				if (map[i][j] == 1) continue; // 벽
+				dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1];
+				
+				if (i == 0) continue;; // -1 불가
+				dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2];
+
+				if (map[i - 1][j] == 1 || map[i][j - 1] == 1) continue; // 대각선 조건
+				dp[i][j][1] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+			}
+		}
+	}
 }
