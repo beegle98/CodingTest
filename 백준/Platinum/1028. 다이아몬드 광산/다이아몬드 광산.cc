4e0bb7492ed2b52ea1a3d619765a0523955e4b8a
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-09-18 15:57:55 +0900

    [Platinum V] Title: 다이아몬드 광산, Time: 44 ms, Memory: 9520 KB -BaekjoonHub

--- 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.cc
+++ 백준/Platinum/1028. 다이아몬드 광산/다이아몬드 광산.cc
@@ -12,12 +12,17 @@ int arr[N][N];
 int main() {
 	//FASTIO
 	cin >> n >> m;
+	// 입력
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			scanf("%1d",&arr[i][j]);
 		}
 	}
 	int ans = 0;
+	// 누적합 계산
+	// ld[x][y] = x,y 좌표에서 왼쪽 아래 대각선으로 1이 이어진 최대 개수
+	// rd[x][y] = x,y 좌표에서 오른쪽 아래 대각선으로 1이 이어진 최대 개수
+	// 아래쪽부터 누적합 계산
 	for(int i=n;i>=1;i--){
 		for(int j=1;j<=m;j++){
 			if(arr[i][j]==1){
@@ -26,11 +31,12 @@ int main() {
 			}
 		}
 	}
+	// k = min(ld, rd)값부터 1까지 다이아몬드가 생성 가능한지 확인한다.
+	// 왼쪽 대각선 끝의 좌표에서 rd 값과 오른쪽 대각선 끝의 좌표에서 ld값이 둘 다 k 이상이면 생성 가능하다.
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			int k = min(ld[i][j],rd[i][j]);
-			//cout << k << '\n';
-			while(k > 0){
+			while(k > ans){
 				if(rd[i+k-1][j-k+1] >= k && ld[i+k-1][j+k-1]>=k){
 					ans = max(ans, k);
 				}
