d8ce42a20eba4a7104ea1b87354c1bfafb3ce8ea
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-05 21:03:18 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+typedef pair<int,int> pi;
+
+int main(){
+    FASTIO
+    int n; cin >> n;
+    cout << (n+1)*2 << " " << (n+1)*3;
+    return 0;
+}
