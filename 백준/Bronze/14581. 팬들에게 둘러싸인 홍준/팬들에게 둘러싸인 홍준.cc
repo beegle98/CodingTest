49d663dc061bf565256276749fd2cc5fc2ccb58b
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-06 11:48:20 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+    FASTIO
+    string s; cin >> s;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::"<<s<<"::fan:\n";
+    cout << ":fan::fan::fan:\n";
+    
+    return 0;
+}
