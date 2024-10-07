a45649db76cc76510ea31978d44c636739a15e43
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-07 21:11:38 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+    FASTIO
+    cout<<"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n";
+    cout<<"N2 Bomber      Heavy Fighter  Limited    21        \n";
+    cout<<"J-Type 327     Light Combat   Unlimited  1         \n";
+    cout<<"NX Cruiser     Medium Fighter Limited    18        \n";
+    cout<<"N1 Starfighter Medium Fighter Unlimited  25        \n";
+    cout<<"Royal Cruiser  Light Combat   Limited    4         \n";
+
+    
+    return 0;
+}
