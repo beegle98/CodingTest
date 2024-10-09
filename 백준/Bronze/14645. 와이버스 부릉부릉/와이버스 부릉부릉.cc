bbf400988dff5effdcf37945405d33da6214b8ad
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-09 16:58:59 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main() {
+    FASTIO
+    cout << "비와이";
+
+    
+    return 0;
+}
