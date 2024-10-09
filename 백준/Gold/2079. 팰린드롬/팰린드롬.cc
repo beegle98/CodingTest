4dff912e8850a0b32859c74e6d0dfa7447907493
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-10 00:11:02 +0900

    [Gold I] Title: 팰린드롬, Time: 12 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/2079. 팰린드롬/팰린드롬.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+#define FASTIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+const int MAX = 1e9;
+string s;
+int dp[2002];
+bool isPalindrome(int l,int r){
+    while(l<=r){
+        if(s[l++]!=s[r--]) return false;
+    }
+    return true;
+}
+int main() {
+    FASTIO
+    cin >> s;
+    int n = s.size();
+    fill(dp,dp+n+1,MAX);
+    dp[0]=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(isPalindrome(j-1,i-1)) {
+				dp[i] = min(dp[i],dp[j-1]+1);
+			}else {
+				dp[i] = min(dp[i],dp[i-1]+1);
+			}
+        }
+    }
+    cout << dp[n];
+    return 0;
+}
