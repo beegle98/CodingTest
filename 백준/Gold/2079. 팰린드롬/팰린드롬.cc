8e08cb35d6874f857d8a9bfc7273a1f6df038d4f
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-10 00:18:39 +0900

    [Gold I] Title: 팰린드롬, Time: 12 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2079. 팰린드롬/팰린드롬.cc
+++ 백준/Gold/2079. 팰린드롬/팰린드롬.cc
@@ -23,11 +23,10 @@ int main() {
     fill(dp,dp+n+1,MAX);
     dp[0]=0;
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
+        dp[i]=dp[i-1]+1;
+        for(int j=1;j<i;j++){
             if(isPalindrome(j-1,i-1)) {
 				dp[i] = min(dp[i],dp[j-1]+1);
-			}else {
-				dp[i] = min(dp[i],dp[i-1]+1);
 			}
         }
     }
