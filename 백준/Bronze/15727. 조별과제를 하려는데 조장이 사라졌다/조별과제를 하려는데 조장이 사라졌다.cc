7f54827aff533f7261b5f3faece693d695a7e7de
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-15 23:28:13 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main(){
+    int n; cin >> n;
+    cout << n/5 + ((n%5)?1:0) ;
+    return 0;
+}
+
