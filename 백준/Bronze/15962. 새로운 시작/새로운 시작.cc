b1eade9ed8f31df938a7174a2ca6f02874f6dc1a
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-16 23:18:21 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main(){
+    cout << "파이팅!!";
+    return 0;
+}
+
