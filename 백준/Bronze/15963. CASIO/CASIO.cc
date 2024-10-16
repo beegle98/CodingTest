2e85de0a15935bd325a8646fc888618edd7cadb4
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-16 23:19:28 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main(){
+    ll n,m; cin >> n >> m;
+    if(n==m) cout<<"1";
+    else cout<<"0";
+    return 0;
+}
+
