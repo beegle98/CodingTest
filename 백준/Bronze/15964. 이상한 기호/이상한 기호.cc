2ee1360c4083db4e1980470cd146f64f6ed1e787
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-16 23:22:50 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main(){
+    int n,m; cin >> n >> m;
+    cout << (n+m)*(n-m);
+    return 0;
+}
+
