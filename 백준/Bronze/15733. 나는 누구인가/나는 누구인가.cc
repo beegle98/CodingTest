f99123a45ea32b9591e2c4e0c396fda908fd51f1
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-16 23:17:51 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main(){
+    cout << "I'm Sexy";
+    return 0;
+}
+
