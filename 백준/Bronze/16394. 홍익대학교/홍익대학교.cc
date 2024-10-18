e5d1fcf65ee6223e5d98175f39e8ae72684dacfd
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-18 23:41:16 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main(){
+    int n; cin >> n;
+    cout << n-1946;
+    return 0;
+}
+
