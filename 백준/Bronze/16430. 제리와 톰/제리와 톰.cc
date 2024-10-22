f991c19f39b761b673178bc8957a8c35c2cad546
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-23 04:11:45 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int a,b; cin >> a >> b;
+    cout << b-a << " " << b;
+    return 0;
+}
+
