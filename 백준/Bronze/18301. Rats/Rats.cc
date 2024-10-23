516b682ada3a4c22fa4ac28ba804ea4ca03a45ec
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-23 23:58:22 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int a,b,c; cin >> a >> b >> c;
+    cout << (a+1)*(b+1)/(c+1)-1;
+
+    return 0;
+}
+
