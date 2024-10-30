92ce4b1f731c74ce27bc554363f0f2b8fe09d990
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-10-30 17:38:09 +0900

    [Gold IV] Title: 서울에서 경산까지, Time: 8 ms, Memory: 2412 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
+++ 백준/Gold/14863. 서울에서 경산까지/서울에서 경산까지.cc
@@ -13,28 +13,38 @@ typedef pair<ll, ll> pll;
 typedef vector<int> vi;
 
 int n, k;
-vector<pi> v[2];
-int ans = 0;
+int dp[100001];
 
-void solve(int idx, int cost, int sum) {
-	if (cost > k) return;
-	if (idx == n) {
-		ans = max(ans, sum);
-		return;
-	}
-	solve(idx + 1, cost + v[0][idx].first, sum + v[0][idx].second);
-	solve(idx + 1, cost + v[1][idx].first, sum + v[1][idx].second);
-}
 int main() {
 	cin >> n >> k;
+	int ans = 0, sum[2] = { 0,0 };
+	vector<int> cost, money;
 	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < 2; j++) {
-			int t, m; cin >> t >> m;
-			v[j].push_back({ t,m });
+		int t1, t2, m1, m2; cin >> t1 >> m1 >> t2 >> m2;
+		if (m1 < m2) {
+			int tmp = m2; m2 = m1; m1 = tmp;
+			tmp = t1; t1 = t2; t2 = tmp;
+		}
+		if (t1 <= t2) {
+			k -= t1;
+			sum[1] += m1;
+			continue;
+		}
+		cost.push_back(t1 - t2);
+		money.push_back(m1 - m2);
+		sum[0] += t2;
+		sum[1] += m2;
+	}
+	
+	k -= sum[0]; // 문제에서 k는 sum[0](cost 합의 최솟값)보다 무조건 크다는 것을 알려줌
+	int N = money.size();
+	for (int i = 0; i < N; i++) {
+		for (int j = k; j >= cost[i]; j--) {
+			dp[j] = max(dp[j], money[i] + dp[j - cost[i]]);
 		}
 	}
-	solve(0, 0, 0);
-	cout << ans;
+	
+	cout << sum[1]+dp[k];
 	return 0;
 }
 
