5fdffa27c7cd69a113538af6a32fa58c95bf3544
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-02 00:29:26 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    ll n; cin >> n;
+    cout << n*n*n;
+    return 0;
+}
+
