19c4f0eb17c5580afde8bfbbec1fb2e670990e65
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-03 14:42:36 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int x; cin >> x;
+    cout << x%21;
+    return 0;
+}
+
