4a0efe826c0dc4efc71fe31d25c693531f5ccaa8
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-04 09:15:33 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+
+
+int main() {
+	char c; cin >> c;
+	if (c == 'N' || c == 'n') {
+		cout << "Naver D2";
+	}
+	else cout << "Naver Whale";
+	return 0;
+}
+
