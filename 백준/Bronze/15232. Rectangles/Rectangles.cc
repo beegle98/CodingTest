9e2854484300dff814cbfd3cefe7c250fb5658e7
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-07 00:48:28 +0900

    [Bronze V] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int n,m; cin >> n >> m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout << '*';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
+
