2031828e1344b552d3397f7cf5f91a4b05153786
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-08 09:06:49 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+
+
+int main() {
+	for (int i = 0; i < 2; i++) {
+		int a, b, c, d, e; cin >> a >> b >> c >> d >> e;
+		cout << a * 6 + b * 3 + c * 2 + d + e * 2 << ' ';
+	}
+
+	return 0;
+}
+
