8211b9114948aa05c65ca127bfd40d8e54cf2328
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-09 19:43:07 +0900

    [Bronze V] Title: 세금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int n; cin >> n;
+    cout << n/100*78 << ' ' << n/1000*956;
+    return 0;
+}
+
