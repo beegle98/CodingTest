e9c559eb34ce9eb4fca946385653d953cb288211
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-10 21:00:34 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int a,b; cin >> a >> b;
+    cout << a*8+b*3-28;
+    return 0;
+}
+
