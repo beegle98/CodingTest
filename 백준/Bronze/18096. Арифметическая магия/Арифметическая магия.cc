e5c6a36584e5286fe81b7d8a3ce454c389349179
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-12 23:59:08 +0900

    [Bronze V] Title: Арифметическая магия, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int n; cin >> n;
+    cout << 1;
+    return 0;
+}
+
