0e67035e5c4ddbee2fc4a370f5c92c67a26628eb
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-15 23:36:18 +0900

    [Bronze V] Title: The Fastest Sorting Algorithm In The World, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23235. The Fastest Sorting Algorithm In The World/The Fastest Sorting Algorithm In The World.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int n;
+    for(int i=1;;i++){
+        cin >> n;
+        if(n==0) break;
+        for(int j=0;j<n;j++){
+            int x; cin >> x;
+        }
+        cout <<"Case " << i <<": Sorting... done!\n";
+    }
+    return 0;
+}
+
