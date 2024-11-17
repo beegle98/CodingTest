2fd1a5e85a79688a3cb46d4e87e7a96cca4e7bb8
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-17 23:22:02 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    string s; cin >> s;
+    if(s=="NLCS") cout << "North London Collegiate School";
+    else if(s=="BHA") cout << "Branksome Hall Asia";
+    else if(s=="KIS") cout << "Korea International School";
+    else if(s=="SJA") cout << "St. Johnsbury Academy";
+}
+
