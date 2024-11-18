e0e0a5595aa4d13ff5b90c5d0c333b55ae0511b1
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-18 23:53:07 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+
+
+int main(){
+    int a,b,c; cin >> a >> b >> c;
+    cout << (b/a)*c*3;
+}
+
