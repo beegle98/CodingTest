1800052816e7f9efd6159fc64a00a94211809128
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-18 09:38:30 +0900

    [Bronze V] Title: SciComLove, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+int main(){
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) cout << "SciComLove\n";
+	return 0;
+}
+
