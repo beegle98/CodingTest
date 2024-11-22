816e839e0cb985d5b5c5138152a32acd593b6630
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-23 00:08:57 +0900

    [Bronze V] Title: Correct, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef vector<int> vi;
+
+int main(){
+    int h,m; cin >> h >> m;
+    cout << (h-9)*60 + m;
+    return 0;
+}
