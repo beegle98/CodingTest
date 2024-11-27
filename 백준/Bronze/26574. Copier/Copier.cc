daf5b4d89e8da0b5eccc6bb995a740850cb06499
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-11-27 17:07:26 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+int main(){
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int x; cin >> x; cout << x << " " << x << '\n';
+	}
+	return 0;
+}
+
