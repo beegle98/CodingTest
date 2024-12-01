615e738ee1db94a8a622c804b4bc018f8730acbe
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-12-02 00:56:18 +0900

    [Bronze V] Title: Serca, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void print() {
+	cout << " @@@   @@@ \n";
+	cout << "@   @ @   @\n";
+	cout << "@    @    @\n";
+	cout << "@         @\n";
+	cout << " @       @ \n";
+	cout << "  @     @  \n";
+	cout << "   @   @   \n";
+	cout << "    @ @    \n";
+	cout << "     @     \n";
+}
+
+int main() {
+	int n; cin >> n;
+	for(int i=0;i<n;i++) print();
+	return 0;
+}
