f5eae93d227a5861fb7abc7c01f5837d232e47f7
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-12-04 23:15:39 +0900

    [Bronze V] Title: 세제곱의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28701. 세제곱의 합/세제곱의 합.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+	int n, sum1 = 0, sum2 = 0; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		sum1 += i;
+		sum2 += i * i * i;
+	}
+	cout << sum1 << '\n' << sum1*sum1 << '\n' << sum2;
+	return 0;
+}
