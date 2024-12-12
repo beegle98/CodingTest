cc99aa99b70eddf8e7a94d2de9cbf031b0896c7e
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-12-13 01:01:04 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+#define FASTIO cin.tie(NULL);cout.tie(NULL);ios_base::sync_with_stdio(false)
+int main() {
+	FASTIO;
+
+	int a, b; cin >> a >> b;
+	cout << a * 10 + b;
+
+	return 0;
+}
