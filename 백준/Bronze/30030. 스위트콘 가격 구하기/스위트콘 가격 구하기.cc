7f0df2960bb917b676b0cfeb092dbdb0cad0eb68
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2024-12-28 14:26:37 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int n; cin >> n;
+    cout << n / 11 * 10;
+    
+    return 0;
+}
