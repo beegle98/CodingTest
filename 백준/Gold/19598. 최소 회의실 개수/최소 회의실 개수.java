1840b953789d2633c8ba5b6b8b5fbeae40cf490e
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2025-01-02 11:37:52 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 676 ms, Memory: 50116 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class Lecture implements Comparable<Lecture>{
+        int st;
+        int ed;
+        Lecture(int st, int ed) {
+            this.st = st;
+            this.ed = ed;
+        }
+        public int compareTo(Lecture o) {
+            if(st==o.st) return ed-o.ed;
+            return st - o.st;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        List<Lecture> s = new ArrayList<>(N);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            s.add(new Lecture(start,end));
+        }
+        Collections.sort(s);
+        for(int i=0;i<N;i++){
+            pq.offer(s.get(i).ed);
+            if(pq.peek() <= s.get(i).st) pq.poll();
+        }
+        System.out.println(pq.size());
+    }
+
+}
+
