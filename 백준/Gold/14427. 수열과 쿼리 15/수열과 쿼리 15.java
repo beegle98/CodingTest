6bb5ff8be6bb53e56cea6a216208c78de3b863d3
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2025-01-03 02:30:31 +0900

    [Gold III] Title: 수열과 쿼리 15, Time: 1160 ms, Memory: 61048 KB -BaekjoonHub

---
+++ 백준/Gold/14427. 수열과 쿼리 15/수열과 쿼리 15.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class A implements Comparable<A>{
+        int idx;
+        int value;
+        A(int idx, int value){
+            this.idx = idx;
+            this.value = value;
+        }
+        public int compareTo(A a){
+            if(this.value == a.value) return this.idx - a.idx;
+            return this.value - a.value;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        PriorityQueue<A> pq = new PriorityQueue<>();
+        List<Integer> s = new ArrayList<>(N+1);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+            s.add(value);
+            pq.add(new A(i,value));
+        }
+        int M = Integer.parseInt(br.readLine());
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int op = Integer.parseInt(st.nextToken());
+            if(op == 1){
+                int idx = Integer.parseInt(st.nextToken());
+                int value = Integer.parseInt(st.nextToken());
+                pq.add(new A(idx-1,value));
+                s.set(idx-1,value);
+            }
+            else{
+                while(!pq.isEmpty() && s.get(pq.peek().idx)!=pq.peek().value){
+                    pq.poll();
+                }
+                System.out.println(pq.peek().idx+1);
+            }
+        }
+
+    }
+
+}
+
