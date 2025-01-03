891bf974d516060e8128ec11072c7bedea8fe950
Jungmun Cha <48426108+beegle98@users.noreply.github.com>
2025-01-03 12:14:57 +0900

    [Bronze V] Title: Metronome, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    double n; cin >> n;
+    printf("%.2lf", n / 4);
+    return 0;
+}
