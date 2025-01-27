3ca8361a57ebf6e13e22acc8b7e2a1b7aebc5e43
Jungmun Cha <ckwjdans4182@gmail.com>
2025-01-27 18:01:42 +0900

    [Bronze V] Title: Micromasters, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    int n; cin >> n;
+    cout << n / 10;
+
+    return 0;
+}
