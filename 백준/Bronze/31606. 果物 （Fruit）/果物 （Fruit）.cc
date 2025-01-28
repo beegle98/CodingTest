1447ca67f732df0811ee930d0e667616befb30fc
Jungmun Cha <ckwjdans4182@gmail.com>
2025-01-28 23:27:34 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    
+    int x, y; cin >> x >> y;
+    cout << x + y + 3;
+
+    return 0;
+}
