a27f2905a5534e83022509915d12f4bf1f6f4d20
Jungmun Cha <ckwjdans4182@gmail.com>
2025-02-03 21:14:10 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll a, b, c; cin >> a >> b >> c;
+    cout << 3 * a + 4 * b + 5 * c;
+    return 0;
+}
