6163c86e4ce3980bfdd511e2857433565cf29bf5
Jungmun Cha <ckwjdans4182@gmail.com>
2025-02-23 00:48:07 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int a, b, c; cin >> a >> b >> c;
+    cout << a * b + c;
+    return 0;
+}
