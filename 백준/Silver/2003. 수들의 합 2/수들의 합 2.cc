a33ab6881c16bbd70332d966317f4291dae55038
Jungmun Cha <ckwjdans4182@gmail.com>
2025-03-06 09:06:40 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2928 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+
+	int n, m; cin >> n >> m;
+	vll sum(n+1);
+	unordered_map<ll, int> h;
+	int ans = 0;
+	h[0] = 1;
+	for (int i = 1;i <= n;i++) {
+		int x; cin >> x;
+		sum[i] = sum[i-1] + x;
+		h[sum[i]] = 1;
+		ans += h[sum[i] - m];
+	}
+	cout << ans;
+
+	return 0;
+}
