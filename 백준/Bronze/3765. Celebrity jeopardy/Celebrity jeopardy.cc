5c94c048cbe614240a5db07556496497f71e69d2
Jungmun Cha <ckwjdans4182@gmail.com>
2025-03-20 01:58:04 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	string s;
+	while (getline(cin, s)) {
+		cout << s << "\n";
+	}
+	return 0;
+}
