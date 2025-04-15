1ae7855fae09ec761e7d8ac7165de8ab8c842874
Jungmun Cha <ckwjdans4182@gmail.com>
2025-04-15 09:17:25 +0900

    [Gold III] Title: 벌레컷, Time: 432 ms, Memory: 129672 KB -BaekjoonHub

--- 백준/Gold/27651. 벌레컷/벌레컷.java
+++ 백준/Gold/27651. 벌레컷/벌레컷.java
@@ -5,32 +5,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Long> a = new ArrayList<>(n);
-        ArrayList<Long> sum = new ArrayList<>(n);
+        long[] a = new long[n+1];
+        long[] sum = new long[n+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        a.add(Long.parseLong(st.nextToken()));
-        sum.add(a.get(0));
 
-        for (int i = 1; i < n; i++) {
-            a.add(Long.parseLong(st.nextToken()));
-            sum.add(sum.get(i - 1) + a.get(i));
+        for (int i = 1; i <= n; i++) {
+            a[i] = Long.parseLong(st.nextToken());
+            sum[i] = sum[i - 1] + a[i];
         }
 
         long ans = 0;
-        for (int i = n - 2; i >= 1; i--) {
-            long valR = sum.get(i) - (sum.get(n - 1) - sum.get(i));
-            int index = Collections.binarySearch(sum, valR);
-            int r = (index < 0) ? -index - 1 : index;
-
-            long valL = sum.get(n - 1) - sum.get(i);
-            index = Collections.binarySearch(sum, valL);
-            int l = (index < 0) ? -index - 1 : index;
-
-            ans += Math.min(r, l);
+        int l=0,r=n-1;
+        while(l<r){
+            while(sum[l+1] < sum[n]-sum[r] && sum[r]-sum[l+1] > sum[n]-sum[r]) l++;
+            //System.out.println(l + " " + r);
+            ans += l;
+            r--;
+            while(l>0 && sum[r]-sum[l] <= sum[n]-sum[r] ) l--;
         }
 
         System.out.println(ans);
     }
 }
-
