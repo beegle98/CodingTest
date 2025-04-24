9f225e8e230d531799e2bf52f90e4de414ce8b17
Jungmun Cha <ckwjdans4182@gmail.com>
2025-04-24 22:30:11 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int n; cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+	return 0;
+}
