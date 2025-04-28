0f0b492d5982d2fad6d400743afbefc2bb8957a2
Jungmun Cha <ckwjdans4182@gmail.com>
2025-04-29 00:46:26 +0900

    [Bronze IV] Title: !밀비 급일, Time: 60 ms, Memory: 11408 KB -BaekjoonHub

--- 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -4,14 +4,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         while(true){
             String s = br.readLine();
             if(s.equals("END")) break;
-            int n = s.length();
-            for(int i=0;i<n;i++){
-                System.out.print(s.charAt(n-i-1));
-            }
-            System.out.println();
+            StringBuilder tmp = new StringBuilder(s);
+            sb.append(tmp.reverse()+"\n");
         }
+        System.out.println(sb);
     }
 }
