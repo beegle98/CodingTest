4cdef507b23b4b97d63ba9066b75a8d29941bb79
Jungmun Cha <ckwjdans4182@gmail.com>
2025-05-20 09:33:03 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	int a, b; cin >> a >> b;
+	cout << b + b - a;
+	return 0;
+}
