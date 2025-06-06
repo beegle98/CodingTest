e5eb0d8ad02ef6a39c8b5d6b84cb6f24fcbcbfe7
Jungmun Cha <ckwjdans4182@gmail.com>
2025-06-06 15:34:18 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int s, t, d; cin >> s >> t >> d;
+	cout << d / (s * 2) * t;
+	return 0;
+}
