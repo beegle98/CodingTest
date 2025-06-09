6cf7afb5a77e002e542302de8eca3bc0b3e0bf69
Jungmun Cha <ckwjdans4182@gmail.com>
2025-06-09 19:34:39 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int a, b, c; cin >> a >> b >> c;
+	cout << max(a + c, b);
+	return 0;
+}
