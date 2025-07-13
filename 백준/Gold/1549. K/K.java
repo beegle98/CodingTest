a10487614428d3ef67e50a578dfd741d2722a192
Jungmun Cha <ckwjdans4182@gmail.com>
2025-07-13 22:39:00 +0900

    [Gold IV] Title: K, Time: 7940 ms, Memory: 13020 KB -BaekjoonHub

--- 백준/Gold/1549. K/K.java
+++ 백준/Gold/1549. K/K.java
@@ -7,26 +7,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int k = 0;
+        int ans = 0;
         long MIN = Long.MAX_VALUE;
         long[] arr = new long[n+1];
         st = new StringTokenizer(br.readLine());
         for(int i=1;i<=n;i++){
-            int x = Integer.parseInt(st.nextToken());
-            arr[i] = arr[i-1] + x;
+            arr[i] = Integer.parseInt(st.nextToken());
+            arr[i] += arr[i-1];
         }
-        for (int i = 1; i <= n / 2; i++) {
-            for (int j = i; j <= n; j++) {
-                for (int l = j + i; l <= n; l++) {
-                    long sub = Math.abs((arr[j] - arr[j - i]) - (arr[l] - arr[l - i]));
+        for (int k = 1; k <= n / 2; k++) {
+            for (int i = k; i <= n; i++) {
+                for (int j = i + k; j <= n; j++) {
+                    long sub = Math.abs((arr[i] - arr[i - k]) - (arr[j] - arr[j - k]));
                     if (sub <= MIN) {
                         MIN = sub;
-                        k = i;
+                        ans = k;
                     }
                 }
             }
         }
-        System.out.println(k);
+        System.out.println(ans);
         System.out.println(MIN);
     }
 }
