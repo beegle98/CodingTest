d2f53c4ee54c16ec2dd95873220777288cf4c6e6
Jungmun Cha <ckwjdans4182@gmail.com>
2025-08-30 15:09:39 +0900

    [Gold I] Title: 구간 합 구하기, Time: 208 ms, Memory: 41092 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,59 +1,67 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 typedef long long ll;
-typedef vector<ll> vll;
 typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
 
-const int N=1000100;
+const int N = 1000100;
 
+vll tree, arr;
 
-vll tree;
+ll init(int node, int s, int e){
+    if (s == e) return tree[node] = arr[s]; //리프 노드
+    int m = (s + e) / 2;
+    return tree[node] = init(node * 2, s, m) + init(node * 2 + 1, m + 1, e);
+}
 
-
-ll sum(int node, int start, int end, int left, int right){
-    if(left > end || right < start) return 0;
-    if(left <= start && right >= end){
+ll sum(int node, int s, int e, int l, int r) {
+    if (l > e || r < s) return 0; // 범위 밖
+    if (l <= s && e <= r) { // 범위 안
         return tree[node];
     }
-    return sum(node*2, start, (start+end)/2, left, right)
-        + sum(node*2+1, (start+end)/2+1, end, left, right);
+    int m = (s + e) / 2;
+    return sum(node * 2, s, m, l, r)
+        + sum(node * 2 + 1, m + 1, e, l, r);
 }
 
-void update(int node, int start, int end, int idx, ll val){
-    if(idx<start || idx > end) return;
-    if(start == end){
-        tree[node]=val;
+void update(int node, int s, int e, int idx, ll val) {
+    if (idx < s || idx > e) return; // 범위 밖
+    if (s == e) { // idx 찾음
+        tree[node] = val;
         return;
     }
-    update(node*2, start, (start+end)/2, idx, val);
-    update(node*2+1, (start+end)/2+1, end, idx, val);
-    tree[node] = tree[node*2]+tree[node*2+1];
+    int m = (s + e) / 2;
+    update(node * 2, s, m, idx, val);
+    update(node * 2 + 1, m + 1, e, idx, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
 }
-int main(){
-    ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int main() {
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-    int n,m,k;
+    int n, m, k;
     cin >> n >> m >> k;
-    
-    tree = vll(4*N);
 
-    for(int i=1;i<=n;i++){
-        ll a; cin >> a;
-        update(1,1,N,i,a);
+    tree = vll(4 * N);
+    arr = vll(N);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        //update(1, 1, n, i, arr[i]);
     }
-    for(int i=0;i<m+k;i++){
-        ll a; cin >> a;
-        if(a==1){//update
-            ll b,c; cin >> b >> c;
-            update(1,1,N,b,c);
+    init(1, 1, n);
+
+    for (int i = 0; i < m + k; i++) {
+        int op; cin >> op;
+        if (op == 1) {// update
+            ll a, b; cin >> a >> b;
+            update(1, 1, n, a, b);
         }
-        else if(a==2){// sum print
-            ll b,c; cin >> b >> c;
-            cout << sum(1,1,N,b,c) <<'\n';
+        else if (op == 2) {// sum
+            ll a, b; cin >> a >> b;
+            cout << sum(1, 1, n, a, b) << '\n';
         }
     }
 
